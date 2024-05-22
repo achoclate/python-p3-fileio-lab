fa5edd830b72b0d506eff03fb6a10e3ac9f99173
achoclate <nyabokeann99@gmail.com>
2024-05-22 12:42:26 +0300

    complete lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,22 @@
-def write_file(file_name, file_content):
-    pass
+# lib/file_io.py
 
-def append_file(file_name, append_content):
-    pass
+def write_file(file_name, file_content):
+    # Add .txt extension to file name
+    file_name = f'{file_name}.txt'
+    with open(file_name, 'w') as f:
+        f.write(file_content)
+
+def append_file(file_name, file_content):
+    # Add .txt extension to file name
+    file_name = f'{file_name}.txt'
+    with open(file_name, 'a') as f:
+        f.write(file_content)
 
 def read_file(file_name):
-    pass
+    # Add .txt extension to file name
+    file_name = f'{file_name}.txt'
+    try:
+        with open(file_name, 'r') as f:
+            return f.read()
+    except FileNotFoundError:
+        return None
